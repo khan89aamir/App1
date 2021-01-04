5a9cf3489b7844ca310fb556d4bc01c1fc14f0a7
AAMIR <khan89aamir@gmail.com>
2021-01-04 18:59:30 +0530

    Add project files.

---
+++ App1/App1/frmHome.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class frmHome : ContentPage
+    {
+        public string strLoginUserName;
+        public frmHome()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        // Load method 
+        protected override void OnAppearing()
+        {
+            lblLoginBy.Text = "Login By : " + strLoginUserName;
+            base.OnAppearing();
+        }
+       
+    }
+}
---
+++ App1/App1/frmLogin.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class frmLogin : ContentPage
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string pass = txtPassword.Text;
+
+            if (ValidateUser(userName, pass))
+            {
+
+                // creating object
+                frmHome ObjHome = new frmHome();
+
+                //setting data
+                ObjHome.strLoginUserName = userName;
+
+                // calling the form
+                App.Current.MainPage= ObjHome; // on apprearing
+
+            }
+            else
+            {
+                 await  DisplayAlert("Login", "Incorrect user name and password", "OK");
+
+               
+            }
+        }
+        private bool ValidateUser(string userName, string pass)
+        {
+            bool result = false;
+            if (userName=="admin" && pass=="123")
+            {
+                result = true;
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        
+        }
+    }
+}
